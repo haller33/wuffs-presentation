a0cba1ada00cef70a4a7d6f7f82051fe2bca6dcc
haller33 <eliseu42lucena@gmail.com>
2023-08-02 23:33:31 -0300

    begin burn

---
+++ build.sh
@@ -0,0 +1,3 @@
+#!/bin/env bash
+
+racket -I slideshow/simple main.rkt
---
+++ main.rkt
@@ -0,0 +1,16 @@
+#lang slideshow/simple
+
+!resources/imgs/banner.jpg
+# this
+
+Wuffs
+# this
+
+But Why Wuffs ?
+# this
+
+File Format Safety
+# this
+
+https://github.com/google/wuffs#readme
+# this
